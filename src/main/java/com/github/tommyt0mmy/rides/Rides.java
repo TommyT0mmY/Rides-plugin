45753034ae33cf18519f598669cab2417e2dfec1
Tommaso Goni <tommasogo99@gmail.com>
2020-03-13 00:02:13 +0100

    [#171779826] Added main class

--- src/main/java/com/github/tommyt0mmy/rides/Rides.java
+++ src/main/java/com/github/tommyt0mmy/rides/Rides.java
@@ -2,6 +2,36 @@ package com.github.tommyt0mmy.rides;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class Rides extends JavaPlugin {
+
+    private static Rides instance;
+    public Logger console = getLogger();
+
+    public static Rides getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Rides instance) {
+        Rides.instance = instance;
+    }
+
+    public void onEnable() {
+        setInstance(this);
+
+    }
+
+    public void onDisable() {
+
+    }
+
+    private void loadCommands() {
+
+    }
+
+    private void loadEvents() {
+
+    }
 
 }
