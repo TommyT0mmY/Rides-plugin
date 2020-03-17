3278e08810a654949ecc12caccf58456a9035857
Tommaso Goni <tommasogo99@gmail.com>
2020-03-17 13:31:29 +0100

    Cleaned code

--- src/main/java/com/github/tommyt0mmy/rides/commands/RidesCommand.java
+++ src/main/java/com/github/tommyt0mmy/rides/commands/RidesCommand.java
@@ -18,32 +18,37 @@ import org.bukkit.persistence.PersistentDataType;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class RidesCommand implements CommandExecutor {
+public class RidesCommand implements CommandExecutor
+{
 
     Rides RidesClass = Rides.getInstance();
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
 
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player))
+        {
             //TODO send only players message
             return true;
         }
 
         Player p = (Player) sender;
 
-        if (args.length != 0) {
+        if (args.length != 0)
+        {
             //TODO send usage message
             return true;
         }
 
-        if (!p.hasPermission("rides.rides")) {
+        if (!p.hasPermission("rides.rides"))
+        {
             //TODO send error message
             return true;
         }
 
         //Opening GUI
-        Inventory inv = Bukkit.createInventory(p, 27, "Rides GUI");
+        Inventory inv = Bukkit.createInventory(p, 27, "§eRides GUI");
         inv.setItem(10, customGUIitem(Material.HORSE_SPAWN_EGG, RidesItemKey.HORSE_LIST_BUTTON, "Spawn Horse", "Lists every horse", "possessed by you"));
         inv.setItem(16, customGUIitem(Material.BARRIER, RidesItemKey.HELP_BUTTON, "Help", "For help digit", "/rideshelp"));
 
@@ -52,14 +57,16 @@ public class RidesCommand implements CommandExecutor {
         return true;
     }
 
-    private ItemStack customGUIitem(Material material, RidesItemKey code, String name, String...lore) {
+    private ItemStack customGUIitem(Material material, RidesItemKey code, String name, String...lore)
+    {
         ItemStack itemstack = new ItemStack(material);
         ItemMeta meta = itemstack.getItemMeta();
         NamespacedKey key = new NamespacedKey(RidesClass, "rides-key");
 
         ArrayList<String> loreList = new ArrayList<>(Arrays.asList(lore));
 
-        if (meta != null) {
+        if (meta != null)
+        {
             meta.getPersistentDataContainer().set(key, PersistentDataType.STRING , code.toString());
             meta.setLore(loreList);
             meta.setDisplayName(name);
